d794e8304d0a464ebd616c4c6be250c278115c10
kpk-i710 <47340012+kpk-i710@users.noreply.github.com>
2024-12-02 21:55:56 +0600

    добавли заказы модель

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
+from email.policy import default
+from random import choice
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Text, Boolean
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey
 from sqlalchemy_utils.types import ChoiceType
+from sqlalchemy.orm import relationship
 
 engine = create_engine('psostgresql://postgres:nathanoj35@localhost/pizza_deliv', echo=True)
 
@@ -16,17 +20,33 @@ class User(Base):
     password = Column(Text, unique=True)
     is_staff = Column(Boolean, default=False)
     is_active = Column(Boolean, default=False)
+    orders = relationship('Order',back_populates='user')
 
     def __repr__(self):
         return f"<User {self.username}"
 
 
-class Choise(Base):
+
+
+class Order(Base):
     ORDER_STATUSES = (('PENDING', 'pending',),
-                      ('IN-TRANSIT','in-trainsit'),
-                      ('DELIVERED','delivered')
+                      ('IN-TRANSIT', 'in-trainsit'),
+                      ('DELIVERED', 'delivered')
                       )
+
+    PIZZA_SIZES = (
+        ('SMALL', ' small'),
+        ('MEDIUM', 'medium'),
+        ('LARGE', 'large'),
+        ('EXTRA-LARGE', 'extra-large'))
 
     __tablename__ = 'orders'
     id = Column(Integer, primary_key=True)
     quantity = Column(Integer, nullable=False)
+    order_status = Column(ChoiceType(choices=ORDER_STATUSES), default="PENDING")
+    pizza_size = Column(ChoiceType(choices=PIZZA_SIZES,default = 'SMALL'))
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship('User',back_populates='urders')
+
+    def __repr__(self):
+        return f"<Order {self.id}"
