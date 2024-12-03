3847f3660ce70f472902a58a4720909b40de08fd
kpk-i710 <47340012+kpk-i710@users.noreply.github.com>
2024-12-03 09:33:46 +0600

    добавил werkzeug

--- auth_routes.py
+++ auth_routes.py
@@ -1,14 +1,40 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter,status
+from database import Session, engine
+from init_db import new_user
+from schemas import SignUpModel
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.exceptions import HTTPException
+from models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth_router = APIRouter()
 
+session = Session(bind=engine)
 
-auth_router=APIRouter(
+auth_router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
 
+
 @auth_router.get('/')
 async def hello():
-    return {"message" : "Hello world"}
+    return {"message": "Hello world"}
+
+
+async def signup(user: SignUpModel, session: AsyncSession):
+    # Проверяем, существует ли пользователь с таким email
+    result = await session.execute(select(User).filter(User.email == user.email))
+    db_email = result.scalar_one_or_none()
+
+    if db_email is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                             detail="User with the email already exists")
+
+    db_email = session.query(User).filter(User.email == user.email).first()
+
+    if db_email is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                             detail="User with the email already exists")
+    new_user = User(username=user.username, email=user, password = )
--- database.py
+++ database.py
@@ -2,11 +2,14 @@ from email.policy import default
 from random import choice
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "mysql+mysqlconnector://p585477_rafael:560200Gram@185.105.110.6/p585477_rafael"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 Base = declarative_base()
+
+# Создание сессии для взаимодействия с базой данных
+Session = sessionmaker(bind=engine)
+session = Session()
